83ab5d4054b0f21a05314bf02a1c7d916c2d5808
rahmoune0768 <rahmoune0768@gmail.com>
2025-04-09 13:50:40 +0100

    minor changes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,12 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="icon" href="/images/favicon.svg" type="image/x-icon">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
         @routes
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,19 @@
 <?php
-
+use App\Http\Controllers\DocumentController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\ReminderController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\QuoteController;
+use App\Http\Controllers\SettingController;
+use App\Http\Controllers\IndustryController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\QuoteItemController;
+use App\Http\Controllers\InvoiceItemController;
+use App\Http\Controllers\LegalStatutController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -22,6 +36,21 @@ Route::get('/', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::resource('documents', DocumentController::class);
+    Route::resource('tasks', TaskController::class);
+    Route::resource('roles', RoleController::class);
+    Route::resource('contacts', ContactController::class);
+    Route::resource('products', ProductController::class);
+    Route::resource('companies', CompanyController::class);
+    Route::resource('reminders', ReminderController::class);
+    Route::resource('categories', CategoryController::class);
+    Route::resource('quotes', QuoteController::class);
+    Route::resource('settings', SettingController::class);
+    Route::resource('industries', IndustryController::class);
+    Route::resource('invoices', InvoiceController::class);
+    Route::resource('quote-items', QuoteItemController::class);
+    Route::resource('invoice-items', InvoiceItemController::class);
+    Route::resource('legal-statuts', LegalStatutController::class);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
